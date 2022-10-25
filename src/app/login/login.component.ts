00702fb609b5822e17d3dabba106f4544371af19
Arne <seidel-arne@web.de>
2022-10-25 22:20:02 +0200

    added user info when password is false when loggin in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { catchError, EMPTY, throwError } from 'rxjs';
 import { Authentication } from '../model/authentication';
 import { UserService } from '../services/user.service';
 
@@ -11,6 +13,8 @@ import { UserService } from '../services/user.service';
 export class LoginComponent implements OnInit {
 
     @Input() setJwt!: Function;
+
+    loginError = false;
 
     username = new FormControl('', {
         validators: [
@@ -42,15 +46,29 @@ export class LoginComponent implements OnInit {
     }
 
     onSubmit(): void {
+        this.loginError = false;
         console.debug('Your login has been submitted', this.loginForm.value);
-        this.userService.authenticate(this.loginForm.value.username!, this.loginForm.value.password!).subscribe((data: Authentication) => {
-            this.userService.setAuth(data);
-            this.userService.broadcastJwtChange(data.accessToken);
-            this.userService.username = this.loginForm.value.username!;
-            this.setJwt(data.accessToken);
-            console.debug('this is the object', this.userService.getAuth())
-        })
-            
+        this.userService.authenticate(this.loginForm.value.username!, this.loginForm.value.password!)
+            .pipe(
+                catchError((error: HttpErrorResponse) => {
+                    console.debug('error in authentication happend', error);
+                    
+                    if(error.status == 403) {
+                        this.loginError = true;
+                        return EMPTY;
+                    }
+                
+                    return throwError(() => new Error('ups something happened'));
+                })
+            ).subscribe((data: Authentication) => {
+                this.loginError = false;
+                this.userService.setAuth(data);
+                this.userService.broadcastJwtChange(data.accessToken);
+                this.userService.username = this.loginForm.value.username!;
+                this.setJwt(data.accessToken);
+                console.debug('this is the object', this.userService.getAuth())
+            })
+
     }
 
 }
